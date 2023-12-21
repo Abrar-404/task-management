7d00f18f4181d6ee5a13bafb65fe8f97c2c6b025
Sahil Abrar <zeroxkhan2@gmail.com>
2023-12-21 19:09:31 +0600

    added footer

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
 
 const Home = () => {
   return (
     <div>
       <div>
         <Banner></Banner>
+        <Footer></Footer>
       </div>
     </div>
   );
